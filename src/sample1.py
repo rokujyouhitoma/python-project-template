767e90b68e9e9c7855800cae6aa9b1470a8f50d4
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2020-12-13 08:56:12 +0900

    Implement of src/sample1.py

--- src/sample1.py
+++ src/sample1.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
-
+import json
 
 raw = '{"name":"this is name"}'
+
+data = json.loads(raw)
+
+
+class JSONSerializer:
+    def serialize(self, data):
+        pass
 
 
 @dataclass
--- tests/test_sample1.py
+++ tests/test_sample1.py
@@ -1,4 +1,4 @@
-import sample
+import sample1
 
 def test_sample():
     assert True
