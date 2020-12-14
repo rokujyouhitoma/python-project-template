cb099fb9f8ddfe7cad2c9d51ab4eab522abb5174
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2020-12-14 21:56:20 +0900

    Update src/sample1.py

--- src/sample1.py
+++ src/sample1.py
@@ -1,16 +1,19 @@
-from dataclasses import dataclass
 import json
+from dataclasses import dataclass
 
 raw = '{"name":"this is name"}'
 
 data = json.loads(raw)
 
 
-class JSONSerializer:
-    def serialize(self, data):
-        pass
-
-
 @dataclass
 class ResponseModel:
     name: str
+
+
+class JSONSerializer:
+    def serialize(self, data):
+        return ResponseModel(data.get("name"))
+
+
+serialized = JSONSerializer().serialize(data)
