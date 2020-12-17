038ab4b9f3279e49ee109b6d09bb4af8fa211130
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2020-12-17 22:00:40 +0900

    Update src/sample1.py

--- src/sample1.py
+++ src/sample1.py
@@ -2,13 +2,20 @@ import json
 from collections.abc import Callable
 from dataclasses import dataclass, fields
 
+# step1: raw
 raw = '{"name":"this is name","age":10}'
 
+# step2: raw to json
 data = json.loads(raw)
 
 
+# step3: json to model
+class Model:
+    pass
+
+
 @dataclass
-class ResponseModel:
+class ResponseModel(Model):
     name: str
     age: int
 
@@ -21,6 +28,17 @@ class JSONSerializer:
         properties = (data.get(field.name) for field in fields(ResponseModel))
         return self.model_class(*properties)
 
+response_model = JSONSerializer(ResponseModel).serialize(data)
+print(response_model)
 
-serialized = JSONSerializer(ResponseModel).serialize(data)
-print(serialized)
+
+# Step4: Entity
+class Entity:
+    def __init__(self, model: Model):
+        for field in fields(model):
+            setattr(self, field.name, getattr(model, field.name))
+
+
+entity = Entity(response_model)
+print(entity.name)
+print(entity.age)
